4f66d9a0a9d5bffa9e8bcd3adbb809cf3b5e23ea
Wojciech Rygielski <rygielski@mimuw.edu.pl>
2012-09-27 13:56:44 +0200

    Initial import.

---
+++ README.md
@@ -0,0 +1,117 @@
+External Problem Marker for Eclipse
+===================================
+
+This plug-in takes `stdout` from the script of your choice, for example:
+
+	views.py:18:101: E501 line too long (102 > 100 characters)
+	views.py:25:101: E501 line too long (105 > 100 characters)
+	... etc ...
+
+And transforms it to Eclipse problem markers:
+
+![Problem markers](http://i.imgur.com/lJqAr.png)
+
+
+Installation
+------------
+
+* Close Eclipse.
+* Download the plugin file from the
+  [downloads](https://github.com/wrygiel/eclipse-external-problem-marker/downloads) section.
+* Put it into the `dropins` directory inside your Eclipse folder.
+
+Then, go to your project directory and edit the `.project` file, add the following sections:
+
+	<buildSpec>
+		<!-- Other, existing builders for your project -->
+		<buildCommand>...</buildCommand>
+		
+		<!-- ADD THIS ONE! Consider leaving the URL behind, for future reference. -->
+		<buildCommand>
+			<!-- About this plugin: https://github.com/wrygiel/eclipse-external-problem-marker -->
+			<name>net.rygielski.eclipse.problemmarker.builder</name>
+			<arguments>
+				<dictionary>
+					<key>filter</key>
+					<value>^.*\.py$</value>
+				</dictionary>
+				<dictionary>
+					<key>command</key>
+					<value>cmd /c python D:\\PRIV\\Projekty\\pep8\\pep8.py "$1"</value>
+				</dictionary>
+				<dictionary>
+					<key>output-match</key>
+					<value>^([A-Z]:)?[^:]+:([0-9]+):([0-9]+): (.)(.*)$</value>
+				</dictionary>
+				<dictionary>
+					<key>line-number</key>
+					<value>$2</value>
+				</dictionary>
+				<dictionary>
+					<key>severity</key>
+					<value>W</value> <!-- or $4 -->
+				</dictionary>
+				<dictionary>
+					<key>message</key>
+					<value>$4$5</value>
+				</dictionary>
+			</arguments>
+		</buildCommand>
+	</buildSpec>
+	
+	<natures>
+		<!-- Other, existing natures for your project -->
+		<nature>...</nature>
+		
+		<!-- ADD THIS ONE! -->
+		<nature>net.rygielski.eclipse.problemmarker.nature</nature>
+	</natures>
+
+Please note, that you must add **both** `<buildCommand>` and `<nature>` elements.
+
+Reopen your project. Right-click on the project and select *Properties -> Builders*.
+A new project builder named *External Problem Marker Builder* should be available.
+Use the checkbox to turn it on/off.
+
+
+Configuration
+-------------
+
+All configuration is done via editing `<arguments>` elements in the `.project` file.
+
+All keys are **required**. Here is what they mean:
+
+  * **filter** (e.g. `^.*\.py$`) - A regular expression which will be used to filter
+    files within your project. Only files which match the filter will be included
+    in the build.
+  * **command** (e.g. `cmd /c python D:\\PRIV\\Projekty\\pep8\\pep8.py "$1"`) -
+    The command to execute upon each of your matched files.
+    * It **should** contain the `$1` wildcard which will be replaced with a
+      file path upon build.
+    * All backslashes within the command **must** be prefixed with another backslash. 
+    * Whenever you rebuild your entire project, the command will be executed N times,
+      where N is the total number of files matching the *filter*. Depending of the
+      performance of your *command*, it might be a lengthy process.
+    * Once built, successive builds are incremental. The command is executed once
+      per each changed file.
+    * All content which the command writes to `stderr` will be reported as
+      error in line 1.
+  * **output-match** (e.g. `^([A-Z]:)?[^:]+:([0-9]+):([0-9]+): (.)(.*)$`) -
+    A regular expression to match lines from the *command*'s `stdout` to. It should
+    match the whole line (so, it should begin with `^` and end with `$`). It should
+    contain nested expressions at least in those places where the line number and
+    warning message should be. Lines which do not match this expression will
+    be ignored.
+  * **line-number** (e.g. `$2`) - `$` character, followed by the index of *output-match*'s
+  nested group expression which contains the line number.
+  * **severity** (e.g. `W` or `$4`) - This should resolve into one of the letters
+    E (error), W (warning) or I (info). If your `command` differentiates between
+    various severites, you may use the `$N` syntax to match the proper nested group
+    expression.
+  * **message** - The message to display along the problem marker.
+
+
+Contact
+-------
+
+Email me at rygielski@mimuw.edu.pl.
---
+++ src/net/rygielski/eclipse/problemmarker/builder/Builder.java
@@ -0,0 +1,265 @@
+package net.rygielski.eclipse.problemmarker.builder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.IResourceDeltaVisitor;
+import org.eclipse.core.resources.IResourceVisitor;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class Builder extends IncrementalProjectBuilder {
+
+	private final static Logger LOGGER = Logger.getLogger(Builder.class
+			.getName());
+
+	class Issue {
+
+		public String message;
+		public int markerSeverity;
+		public int lineNumber;
+
+		public Issue(int markerSeverity, int lineNumber, String message) {
+			super();
+			this.message = message;
+			this.markerSeverity = markerSeverity;
+			this.lineNumber = lineNumber;
+		}
+
+	}
+
+	class Config {
+		public Pattern filterPattern;
+		public Pattern outputLinePattern;
+		public String lineNumberReplacement;
+		public String messageReplacement;
+		public String severityReplacement;
+		public String command;
+	}
+
+	private Config config;
+
+	private Config getConfig() {
+		if (config == null) {
+			String MSG = "Missing or invalid builder configuration.\n"
+					+ "Please read the docs at: https://github.com/wrygiel/eclipse-external-problem-marker";
+			Map<String, String> arguments = this.getCommand().getArguments();
+			this.config = new Config();
+			this.config.command = arguments.get("command");
+			if (this.config.command == null)
+				throw new RuntimeException(MSG);
+			String filterRegexp = arguments.get("filter");
+			if (filterRegexp == null)
+				throw new RuntimeException(MSG);
+			this.config.filterPattern = Pattern.compile(filterRegexp);
+			String outputLineRegexp = arguments.get("output-match");
+			if (outputLineRegexp == null)
+				throw new RuntimeException(MSG);
+			this.config.outputLinePattern = Pattern.compile(outputLineRegexp);
+			this.config.lineNumberReplacement = arguments.get("line-number");
+			if (this.config.lineNumberReplacement == null)
+				throw new RuntimeException(MSG);
+			this.config.messageReplacement = arguments.get("message");
+			if (this.config.messageReplacement == null)
+				throw new RuntimeException(MSG);
+			this.config.severityReplacement = arguments.get("severity");
+			if (this.config.severityReplacement == null)
+				throw new RuntimeException(MSG);
+		}
+		return config;
+	}
+
+	class DeltaVisitor implements IResourceDeltaVisitor {
+		public boolean visit(IResourceDelta delta) throws CoreException {
+			IResource resource = delta.getResource();
+			switch (delta.getKind()) {
+			case IResourceDelta.ADDED:
+				// handle added resource
+				addMarkersToResource(resource);
+				break;
+			case IResourceDelta.REMOVED:
+				// handle removed resource
+				break;
+			case IResourceDelta.CHANGED:
+				// handle changed resource
+				addMarkersToResource(resource);
+				break;
+			}
+			// return true to continue visiting children.
+			return true;
+		}
+	}
+
+	class ResourceVisitor implements IResourceVisitor {
+		public boolean visit(IResource resource) {
+			addMarkersToResource(resource);
+			// return true to continue visiting children.
+			return true;
+		}
+	}
+
+	public static final String BUILDER_ID = "net.rygielski.eclipse.problemmarker.builder";
+
+	private static final String MARKER_TYPE = "net.rygielski.eclipse.problemmarker.problem";
+
+	protected IProject[] build(int kind, Map<String,String> args, IProgressMonitor monitor)
+			throws CoreException {
+		if (kind == FULL_BUILD) {
+			fullBuild(monitor);
+		} else {
+			IResourceDelta delta = getDelta(getProject());
+			if (delta == null) {
+				fullBuild(monitor);
+			} else {
+				incrementalBuild(delta, monitor);
+			}
+		}
+		return null;
+	}
+
+	void addMarkersToResource(IResource resource) {
+		LOGGER.log(Level.INFO, "Trying " + resource.getName() + "...");
+		if (resource instanceof IFile
+				&& getConfig().filterPattern.matcher(resource.getName()).find()) {
+			IFile file = (IFile) resource;
+			deleteMarkers(file);
+			List<Issue> issues = getIssues(file.getRawLocation().makeAbsolute());
+			try {
+				for (Issue i : issues) {
+					IMarker marker = file.createMarker(MARKER_TYPE);
+					marker.setAttribute(IMarker.MESSAGE, i.message);
+					marker.setAttribute(IMarker.SEVERITY, i.markerSeverity);
+					marker.setAttribute(IMarker.LINE_NUMBER, i.lineNumber);
+				}
+			} catch (CoreException e) {
+			}
+		}
+	}
+
+	private List<Issue> getIssues(IPath fullPath) {
+		LOGGER.log(Level.INFO, "Will generate issues for " + fullPath);
+		List<Issue> issues = new ArrayList<Issue>();
+		String line;
+		Process p;
+		String command = fullPath.makeAbsolute().toOSString()
+				.replaceAll("^(.*)$", getConfig().command);
+		try {
+			LOGGER.log(Level.INFO, "Execute: " + command);
+			p = Runtime.getRuntime().exec(command);
+		} catch (IOException e1) {
+			issues.add(new Issue(IMarker.SEVERITY_ERROR, 1,
+					"Couldn't execute: " + command + "\n" + e1.getMessage()));
+			return issues;
+		}
+
+		BufferedReader bri = new BufferedReader(new InputStreamReader(
+				p.getInputStream()));
+		BufferedReader bre = new BufferedReader(new InputStreamReader(
+				p.getErrorStream()));
+		try {
+			while ((line = bri.readLine()) != null) {
+				Issue issue = readIssue(line);
+				if (issue != null)
+					issues.add(issue);
+			}
+			bri.close();
+			while ((line = bre.readLine()) != null) {
+				issues.add(new Issue(IMarker.SEVERITY_ERROR, 1, "stderr: "
+						+ line));
+			}
+			bre.close();
+		} catch (IOException e) {
+			issues.add(new Issue(IMarker.SEVERITY_ERROR, 1, "I/O error: "
+					+ e.getMessage()));
+			return issues;
+		}
+
+		try {
+			p.waitFor();
+		} catch (InterruptedException e) {
+			issues.add(new Issue(IMarker.SEVERITY_ERROR, 1,
+					"Command interrupted: " + e.getMessage()));
+			return issues;
+		}
+
+		return issues;
+	}
+
+	private Issue readIssue(String line) {
+		LOGGER.log(Level.INFO, "Parsing line: " + line);
+		try {
+			Matcher matcher = getConfig().outputLinePattern.matcher(line);
+			if (matcher.find()) {
+				int lineNumber = Integer.parseInt(matcher
+						.replaceAll(getConfig().lineNumberReplacement));
+				String message = matcher
+						.replaceAll(getConfig().messageReplacement);
+				String severityStr = matcher
+						.replaceAll(getConfig().severityReplacement);
+				int markerSeverity = IMarker.SEVERITY_ERROR;
+				if (severityStr.startsWith("W"))
+					markerSeverity = IMarker.SEVERITY_WARNING;
+				else if (severityStr.startsWith("E"))
+					markerSeverity = IMarker.SEVERITY_ERROR;
+				else if (severityStr.startsWith("I"))
+					markerSeverity = IMarker.SEVERITY_INFO;
+				LOGGER.log(Level.INFO, "Line matched: " + severityStr + "@"
+						+ lineNumber + " " + message);
+				Issue issue = new Issue(markerSeverity, lineNumber, message);
+				return issue;
+			}
+		} catch (RuntimeException e) {
+			return new Issue(IMarker.SEVERITY_ERROR, 1,
+					"Error when matching line: " + line + "\n" + e.getMessage());
+		}
+		return null;
+	}
+
+	private void deleteMarkers(IFile file) {
+		try {
+			file.deleteMarkers(MARKER_TYPE, false, IResource.DEPTH_ZERO);
+		} catch (CoreException ce) {
+		}
+	}
+
+	protected void fullBuild(final IProgressMonitor monitor)
+			throws CoreException {
+		setupLogger();
+		try {
+			getProject().accept(new ResourceVisitor());
+		} catch (CoreException e) {
+		}
+	}
+
+	void setupLogger() {
+		/*
+		 * if (LOGGER.getHandlers().length > 0) return;
+		 * LOGGER.setLevel(Level.ALL); FileHandler fileTxt; try { fileTxt = new
+		 * FileHandler("D:\\log.txt"); } catch (IOException e) { throw new
+		 * RuntimeException(e); } SimpleFormatter formatterTxt = new
+		 * SimpleFormatter(); fileTxt.setFormatter(formatterTxt);
+		 * LOGGER.addHandler(fileTxt);
+		 */
+	}
+
+	protected void incrementalBuild(IResourceDelta delta,
+			IProgressMonitor monitor) throws CoreException {
+		setupLogger();
+		delta.accept(new DeltaVisitor());
+	}
+}
